115c44c266f45498983a16a882d263ab39cfd55e
Бернова Анна <bast33@yandex.ru>
2019-08-21 22:41:34 +0300

    Add html for map block

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,14 @@
 let item = document.querySelector(".testimonials__item-text");
 let  showBtn= document.querySelector(".testimonials__item-read");
-let  testimonial= document.querySelector(".testimonials__item");
+//let  testimonial= document.querySelector(".testimonials__item");
+let  testimonial= document.querySelector(".testimonials__item-wrapper");
 
 showBtn.addEventListener("click", function(evt){
   item.style.overflow = "visible"; 
- 
   showBtn.style.display = "none"; 
+  item.style.height = item.scrollHeight+"px";
+  let computedStyle = getComputedStyle(document.testimonial);
+  alert(computedStyle);
+  let testimonialBgHeight = computedStyle+parseInt(item.scrollHeight);
+  testimonial.style.height = testimonialBgHeight +"px";
  });
