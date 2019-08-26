e64618ec36a660d4f8b24396dd19df9d4274e4a5
Бернова Анна <bast33@yandex.ru>
2019-08-26 21:37:32 +0300

    add all styles for footer

--- app/js/common.babel.js
+++ app/js/common.babel.js
@@ -1,11 +1,15 @@
 "use strict";
 
-var item = document.querySelector(".item__text");
-var showBtn = document.querySelector(".item-read");
-var testimonial = document.querySelector(".testimonials__item");
+var item = document.querySelector(".testimonials__item-text");
+var showBtn = document.querySelector(".testimonials__item-read");
+//let  testimonial= document.querySelector(".testimonials__item");
+var testimonial = document.querySelector(".testimonials__item-wrapper");
 
 showBtn.addEventListener("click", function (evt) {
   item.style.overflow = "visible";
-
   showBtn.style.display = "none";
+  item.style.height = item.scrollHeight + "px";
+  var computedStyle = getComputedStyle(document.testimonial);
+  var testimonialBgHeight = computedStyle + parseInt(item.scrollHeight);
+  testimonial.style.height = testimonialBgHeight + "px";
 });
