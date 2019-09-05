1de1d3c7cf18c4425e5f8dc393dee45fc049c22e
Бернова Анна <bast33@yandex.ru>
2019-09-05 21:54:21 +0300

    add js for opening/closing popup on click

--- app/js/common.babel.js
+++ app/js/common.babel.js
@@ -1,15 +1,38 @@
 "use strict";
 
-var item = document.querySelector(".testimonials__item-text");
-var showBtn = document.querySelector(".testimonials__item-read");
-//let  testimonial= document.querySelector(".testimonials__item");
-var testimonial = document.querySelector(".testimonials__item-wrapper");
+(function () {
 
-showBtn.addEventListener("click", function (evt) {
-  item.style.overflow = "visible";
-  showBtn.style.display = "none";
-  item.style.height = item.scrollHeight + "px";
-  var computedStyle = getComputedStyle(document.testimonial);
-  var testimonialBgHeight = computedStyle + parseInt(item.scrollHeight);
-  testimonial.style.height = testimonialBgHeight + "px";
-});
+  var item = document.querySelector(".testimonials__item-text");
+  var showBtn = document.querySelector(".testimonials__item-read");
+  //let  testimonial= document.querySelector(".testimonials__item");
+  var testimonial = document.querySelector(".testimonials__item-wrapper");
+
+  showBtn.addEventListener("click", function (evt) {
+    item.style.overflow = "visible";
+    showBtn.style.display = "none";
+    item.style.height = item.scrollHeight + "px";
+    var computedStyle = getComputedStyle(document.testimonial);
+    var testimonialBgHeight = computedStyle + parseInt(item.scrollHeight);
+    testimonial.style.height = testimonialBgHeight + "px";
+  });
+})();
+
+// popup
+
+(function () {
+
+  var modal = document.querySelector(".popup");
+  var modalOverlay = document.querySelector(".popup-overlay");
+  var closeButton = document.querySelector(".popup-close");
+  var openButton = document.querySelector(".risk-button");
+
+  closeButton.addEventListener("click", function () {
+    modal.classList.toggle("closed");
+    modalOverlay.classList.toggle("closed");
+  });
+
+  openButton.addEventListener("click", function () {
+    modal.classList.toggle("closed");
+    modalOverlay.classList.toggle("closed");
+  });
+})();
--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,5 @@
+(function () {
+
 let item = document.querySelector(".testimonials__item-text");
 let  showBtn= document.querySelector(".testimonials__item-read");
 //let  testimonial= document.querySelector(".testimonials__item");
@@ -11,3 +13,33 @@ showBtn.addEventListener("click", function(evt){
   let testimonialBgHeight = computedStyle+parseInt(item.scrollHeight);
   testimonial.style.height = testimonialBgHeight +"px";
  });
+
+
+}());
+
+     // popup
+
+(function () {
+
+  let modal = document.querySelector(".popup");
+  let modalOverlay = document.querySelector(".popup-overlay");
+  let closeButton = document.querySelector(".popup-close");
+  let openButton = document.querySelectorAll(".popupBtn");
+  
+  closeButton.addEventListener("click", function() {
+    modal.classList.toggle("closed");
+    modalOverlay.classList.toggle("closed");
+  });
+  
+  openButton.forEach(function (button) {
+  button.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal.classList.toggle("closed");
+    modalOverlay.classList.toggle("closed");
+  })
+  });
+  
+
+}());
+
+  
